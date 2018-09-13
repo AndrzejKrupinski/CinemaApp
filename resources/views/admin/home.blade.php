b67708579eeb0aa3e9c4957d5693d646076b9227
AndrzejKrupinski <andrzej_krupinski@wp.pl>
2018-09-13 21:49:04 +0200

    Added details to admin view

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -6,17 +6,34 @@
         <div class="sidebar-header">
             <ul class="list-unstyled components">
                 <li class="active">
-                    <a href="#homeSubmenu" data-toggle="collapse"
+                    <a href="#cinemaSubmenu" data-toggle="collapse"
                         aria-example="false" class="dropdown-toggle">
-                        <i class="fas fa-home"></i>Home
+                        <i class="fas fa-industry"></i> Manage cinemas
                     </a>
-                    <ul class="collapse list-unstyled" id="homeSubmenu">
-                        <li><a href="#">Cinemas</a>
-                        <li><a href="#">CinemaHalls</a>
-                        <li><a href="#">FilmShows</a>
-                        <li><a href="#">Movies</a>
-                        <li><a href="#">Reservations</a>
-                        <li><a href="#">Users</a>
+                    <ul class="collapse list-unstyled" id="cinemaSubmenu">
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-place-of-worship"></i>
+                                 Cinemas
+                            </a>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-tv"></i> CinemaHalls
+                            </a>
+                        <li>
+                            <a href="#">
+                                <i class="fas fa-play"></i> FilmShows
+                            </a>
+                        <li>
+                            <a href="#">
+                                <i class="far fa-calendar-alt"></i> Reservations
+                            </a>
+                    </ul>
+                    <ul class="list-unstyled" id="movies">
+                        <a href="#"><i class="fas fa-film"></i> Manage movies</a>
+                    </ul>
+                    <ul class="list-unstyled" id="users">
+                        <a href="#"><i class="fas fa-users"></i> Manage users</a>
                     </ul>
                 </li>
             </ul>
@@ -26,8 +43,7 @@
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
                 <button type="button" id="sidebarCollapse" class="btn btn-info">
-                    <i class="fas fa-align-left"></i>
-                    <span>Toggle Sidebar</span>
+                    <i class="fas fa-bars"></i>
                 </button>
             </div>
         </nav>
