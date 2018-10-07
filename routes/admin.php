9ea61bebf44bb6e056cb6c79acf24d9d9f43f638
AndrzejKrupinski <andrzej_krupinski@wp.pl>
2018-10-07 22:19:59 +0200

    refactor: group cinemaroutes in routes/admin.php

--- routes/admin.php
+++ routes/admin.php
@@ -2,9 +2,13 @@
 
 Route::get('/', 'HomeController@index')->name('admin.app');
 
-Route::get('/cinema', 'CinemaController@index')->name('cinema.index');
-Route::get('/cinema/create', 'CinemaController@create')->name('cinema.create');
-Route::post('/cinema', 'CinemaController@store')->name('cinema.store');
-Route::get('/cinema/{cinemaId}/edit', 'CinemaController@edit')->name('cinema.edit');
-Route::put('/cinema/{cinemaId}', 'CinemaController@update')->name('cinema.update');
-Route::delete('/cinema/{cinemaId}', 'CinemaController@destroy')->name('cinema.destroy');
+Route::prefix('cinema')->group(function () {
+    Route::get('/', 'CinemaController@index')->name('cinema.index');
+    Route::get('create', 'CinemaController@create')->name('cinema.create');
+    Route::post('/', 'CinemaController@store')->name('cinema.store');
+    Route::get('{cinemaId}/edit', 'CinemaController@edit')->name('cinema.edit');
+    Route::put('{cinemaId}', 'CinemaController@update')->name('cinema.update');
+    Route::delete('{cinemaId}', 'CinemaController@destroy')->name('cinema.destroy');
+});
+
+Route::get('/movie', 'MovieController@index')->name('movie.index');
